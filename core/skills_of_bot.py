47b14247078ab6303cac032030126a060441ba25
Денис Гришанков <ben300300@gmail.com>
2023-03-09 15:50:25 +0300

    added weather

--- core/skills_of_bot.py
+++ core/skills_of_bot.py
@@ -4,7 +4,7 @@ import time
 import os
 import webbrowser
 
-
+import requests
 import sounddevice as sd
 from fuzzywuzzy import fuzz
 from num_to_rus import Converter
@@ -33,7 +33,7 @@ def respond(voice: str):
         cmd = recognize_cmd(filter_cmd(voice))
 
         if cmd['cmd'] not in commands.DATA_SET.keys():
-            speak("Чтоо?")
+            speak("Чтоо случилось?")
         else:
             execute_cmd(cmd['cmd'])
 
@@ -77,6 +77,7 @@ def execute_cmd(cmd: str):
         elif cmd == 'edge':
             edge_path = os.environ['EDGE']
             webbrowser.open(edge_path)
+        speak("Удачи в поиске чумба!")
 
     elif cmd == 'ctime':
         now = datetime.datetime.now()
@@ -86,8 +87,24 @@ def execute_cmd(cmd: str):
     elif cmd == 'music':
         spotify_path = os.environ['SPOTIFY']
         subprocess.Popen(spotify_path)
+        speak("Приятного прослушивания !")
+
+    elif cmd == "weather":
+        try:
+            params = {'q': 'Minsk', 'units': 'metric', 'lang': 'ru', 'appid': '82b6d937032b30ff2cc9bdcfc6b0771b'}
+            response = requests.get(f'https://api.openweathermap.org/data/2.5/weather', params=params)
+            if not response:
+                raise
+            w = response.json()
+            speak(f"На улице {w['weather'][0]['description']} {round(w['main']['temp'])} градусов")
+
+        except:
+            speak('Произошла ошибка при попытке запроса к ресурсу API, проверь код')
+
+
 
     elif cmd == 'stop':
+        speak("Покааааааааа!!!")
         sd.stop()
 
 
